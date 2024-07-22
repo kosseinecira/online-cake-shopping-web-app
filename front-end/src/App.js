eb1b9f4c1412e3100ef4bd67f6fb2b1c16d6e70e
Kossei Necira <44173226+kosseinecira@users.noreply.github.com>
2024-07-22 12:04:04 +0100

    creating front-end part with react js

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import "./app.css";
+import Header from "./main/global/Header";
+import Footer from "./main/global/Footer";
+import Content from "./main/Content";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
